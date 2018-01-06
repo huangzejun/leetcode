8f4945492629946318224f300cd9994de15d0b37
HUANGZEJUN <592607657@qq.com>
2018-01-06 14:09:20 +0800

    add data structures package

---
+++ src/HouseRobberIII.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HouseRobberIII {
+	public static void main(String[] args){
+		Map<Integer, Integer> map = new HashMap<>();
+		System.out.print(0 + map.get(null));
+	}
+}
